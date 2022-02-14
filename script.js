bce856b7b48f5ae58a586b3bacc69405a3b53083
JSubba <prolearn1000@protonmail.com>
2022-02-14 10:01:23 +0900

    changes made

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const menuToggle = document.querySelector(".toggle");
 const showcase = document.querySelector(".showcase");
-const cruiseHeading = document.querySelector(".cruise-heading");
-const cruiseCardDisplay = document.querySelector(".cruise-cards");
+const destinationDOM = document.getElementById("destinations");
 
 menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   showcase.classList.toggle("active");
 });
 
-function cruiseExplore() {
-  cruiseHeading.style.display = "block";
-  cruiseCardDisplay.style.display = "flex";
-}
+// data fetch
+fetch("destinations.json")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let destinationResorts = data.destinations;
+
+    displayDestinations = "";
+    destinationResorts = destinationResorts.map((item) => {
+      const { name, description, image } = item;
+
+      displayDestinations += `
+                <div class="card">
+                  <a href="#"
+                    ><img
+                      src="${image}"
+                      class="card-img-top"
+                      alt="destination-image"
+                  /></a>
+                  <div class="card-body">
+                    <h5 class="card-title">${name}</h5>
+                    <hr />
+                    <p class="card-text text-start">
+                    ${description}
+                    </p>
+                  </div>
+                </div>
+        `;
+
+      destinationDOM.innerHTML = displayDestinations;
+    });
+  });
