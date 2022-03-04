d445851b0600d8a9ac0cfd70b8d6451f5e04889b
JSubba <prolearn1000@protonmail.com>
2022-03-04 11:51:24 +0900

    contents added and changes made

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const menuToggle = document.querySelector(".toggle");
-const showcase = document.querySelector(".showcase");
+// destination data
 const destinationDOM = document.getElementById("destinations");
+const videoDOM = document.getElementById("videos");
 
-menuToggle.addEventListener("click", () => {
-  menuToggle.classList.toggle("active");
-  showcase.classList.toggle("active");
-});
-
-// data fetch
 fetch("destinations.json")
   .then((response) => {
     return response.json();
@@ -17,26 +11,42 @@ fetch("destinations.json")
 
     displayDestinations = "";
     destinationResorts = destinationResorts.map((item) => {
-      const { name, description, image } = item;
+      const { name, description, image, video } = item;
 
       displayDestinations += `
-                <div class="card">
-                  <a href="#"
-                    ><img
-                      src="${image}"
-                      class="card-img-top"
-                      alt="destination-image"
-                  /></a>
-                  <div class="card-body">
-                    <h5 class="card-title">${name}</h5>
-                    <hr />
-                    <p class="card-text text-start">
-                    ${description}
-                    </p>
-                  </div>
-                </div>
-        `;
+        <div class="card">
+          <a href="#"
+            ><img
+              src="${image}"
+              class="card-img-top"
+              alt="destination-image"
+          /></a>
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <hr />
+            <p class="card-text text-start">
+            ${description}
+            </p>
+          </div>
+        </div>
+      `;
 
       destinationDOM.innerHTML = displayDestinations;
     });
   });
+
+// video play
+let video = document.querySelectorAll("video");
+
+video.forEach((play) =>
+  play.addEventListener("click", () => {
+    play.classList.toggle("active");
+
+    if (play.paused) {
+      play.play();
+    } else {
+      play.pause();
+      play.currentTime = 0;
+    }
+  })
+);
